06193693a4d95dbf6e48b64160f7f53fc6d9d636
Teng Zhang <zhangt2333@gmail.com>
2021-04-30 10:21:41 +0800

    init

---
+++ .github/ISSUE_TEMPLATE/hw_submit.md
@@ -0,0 +1,14 @@
+---
+name: Submit Homework
+about: Submit your homework
+title: 'homework: github repo fork.'
+labels: ''
+assignees: ''
+
+---
+
+**The URL of the repo you fork:**
+
+My URL is: 'https://github.com/SDUOJ-Team/hw-githubRepoFork'
+
+Edit the URL in the quote above!
---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# Copyright 2021 zhangt2333. All Rights Reserved.
+# Author-Github: github.com/zhangt2333
+
+import sys
+import requests
+import re
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+    url = re.findall(r"https://github.com/(.*?)'", sys.argv[1])[0]
+    response = requests.get("https://github.com/" + url)
+    soup = BeautifulSoup(response.text, features='lxml')
+    fork_path = soup.find('span', {'class': 'text-small lh-condensed-ultra no-wrap mt-1'}).text.strip()
+    sys.exit(0 if 'forked from SDUOJ-Team/hw-githubRepoFork' in fork_path else -1)
+
+
+
